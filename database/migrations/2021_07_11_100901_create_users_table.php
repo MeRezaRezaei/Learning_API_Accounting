9534abcc0fddca60a0524af583ac5f5508ff982b
Reza Rezaei <merezarezaei@gmail.com>
2021-07-12 12:19:44 +0430

    user table and model added

---
+++ database/migrations/2021_07_11_100901_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name',60);
+            $table->string('last_name',60);
+            $table->string('phone',15);
+            $table->string('email',100)->unique();
+            $table->boolean('gender');
+            $table->timestamp('lsat_login_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
